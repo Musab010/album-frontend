a4c7d1d07c340dec6c8bd09a13e38808247d9e55
msbb010 <60715167+msbb010@users.noreply.github.com>
2024-06-12 15:12:06 +0200

    error fix

--- src/hooks/useAlbum.js
+++ src/hooks/useAlbum.js
@@ -1,25 +1,27 @@
 import { useEffect, useState } from "react";
+
 const endpoint = process.env.REACT_APP_API_URL + '/api/album/';
 
 const useAlbum = (id) => {
-    const [album, setAlbum ] = useState(null);
-    if(id == "new"){
+    const [album, setAlbum] = useState(null);
+
+    // Gebruik strikt gelijk aan '===' in plaats van '=='
+    if(id === "new"){
         id = "";
     }
+
     useEffect(() => {
-        fetch(endpoint +  id,
-            )
+        fetch(endpoint + id)
             .then(async response => {
                 if (!response.ok) {
                     return;
                 }
-                
-                setAlbum(await response.json())
+                setAlbum(await response.json());
             })
             .catch(error => console.error(error));
-    }, []);
+    }, [id]); // Voeg 'id' toe aan de dependency array
+
     return album;
 }
-
 
 export default useAlbum;
